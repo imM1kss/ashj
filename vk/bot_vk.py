b16dfc9517ca5a33f462030814b2f3eac5ebd8b8
imM1kss <slavmikerin45@gmail.com>
2026-03-14 20:16:44 +0500

    just a comments

--- vk/bot_vk.py
+++ vk/bot_vk.py
@@ -18,10 +18,7 @@ dotenv.load_dotenv()
 TOKEN = os.getenv("VK_token")
 GROUP_ID = os.getenv("group_id")
 
-# send message func -> None
-async def send_message(id: Optional[int] = None,
-                       message: Optional[str] = None,) -> None:
-    pass
+
 
 #run function -> None
 async def main_vk() -> None:
@@ -31,24 +28,40 @@ async def main_vk() -> None:
         # init longpoll
         long_poll = BotsLongPoll(api, group_id=GROUP_ID)
 
-        print("Бот на aiovk погнал!")
+        #send message func -> None
+        async def send_message(id:Optional[int] = None,
+                               message:Optional[str] = None,
+                               keyboard:Optional[str] = None,
+                               attachments:Optional[List] = None) -> None:
+            #transform message to "" or don't transform it
+            message = message or ""
+            #if id is not defineded -> ValueError
+            if id is None:
+                raise ValueError("Id is not defineded")
 
-        # Цикл прослушки событий
-        async for event in long_poll.iter():
-            # Проверяем, что это новое сообщение
-            if event['type'] == 'message_new':
-                message = event['object']['message']
-                user_id = message['from_id']
-                peer_id = message['peer_id']
-                messagetext = message['text']
-
-                # Отвечаем тем же текстом
-                await api.messages.send(
-                    peer_id=peer_id,
-                    message=f"Эхо: {messagetext}",
-                    random_id=0
+            #await method vk api for send message (id,message_text,keyboard,attacments,random_id(0 -> 10000))
+            await api.messages.send(
+                    peer_id=id,
+                    message=message,
+                    keyboard = keyboard,
+                    attachments = attachments,
+                    random_id=random.randint(0,10000)
                 )
 
+        
+        print("Бот на aiovk погнал!")
+
+        # Cycle listenning events
+        async for event in long_poll.iter():
+            # evevnt is "new message"
+            if event['type'] == 'message_new':
+                message = event['object']['message'] # message object
+
+                user_id = message['from_id'] # sender's user id
+                peer_id = message['peer_id'] # sender's chat id 
+                messagetext = message['text'] # text message
+
+#testing if run directly
 if __name__ == "__main__":
     try:
         asyncio.run(main_vk())
